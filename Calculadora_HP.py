7908b48272f17b06d575fefc1febddcbecff4c20
Apollo-Hb <apollohb0@gmail.com>
2024-05-13 17:31:47 -0300

    commit

--- Calculadora_HP.py
+++ Calculadora_HP.py
@@ -4,15 +4,19 @@ while True:
     print("- - Subtração")
     print("* - Multiplicação")
     print("/ - Divisão")
+    print("X - Sair")
     opcao = input("Escolha uma opção: ")
+
+    if opcao == 'X':
+        break                               #Conferindo se o usuario parou o loop(programa) ou escolheu uma das opções.
 
     resultado = float(input("Digite qualquer número ou 'P' para encerrar o calculo: "))
 
     while True:
         numero = input("Digite 'P' para parar: ")
-        if numero == 'P':                                   #Conferindo se o usuario parou o loop(programa) ou escolheu uma das opções.
-            break
-        else:                                               #Iniciando um outro loop infinito e esperando o usuario escolher uma opção e lendo os numeros e até o usuario digitar P para parar a programação ou continuar a operação digitando mais um numero.
+        if numero == 'P':
+            break                                           #Iniciando outro loop infinito e esperando o usuario escolher uma opção e lendo os numeros e até o usuario digitar P ou continuar a operação digitando mais um número.
+        else:
             
             numero = float(numero)
 
